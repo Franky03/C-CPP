2ae0d275f6bc4347b18488bdb4461859502dc2c3
Franky03 <kaikym20@gmail.com>
2023-02-09 16:15:13 -0300

    config cpp

--- C++/hello.cpp
+++ C++/hello.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main(){   
     
-    cout << "Hello World" << endl;
+    auto result  = (10 <=> 20) > 0;
+    cout << result << endl;
 
     return 0;
 }

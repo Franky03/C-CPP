ba031dd42666712b8b3cdf2871ba00dbdf442aac
Franky03 <kaikym20@gmail.com>
2023-10-09 20:58:17 -0300

    edit

--- Codes/executecountone.c
+++ Codes/executecountone.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 
 int main(void){
-    char* binaries_to_test[] = {"0000", "0001", "0010", "1111", "0100", "0101", "0110", "0111"};
+    char* binaries_to_test[] = {"00000000", "00000001", "00000010", "00000011", "00000100", "11110101", "01010110", "00000111", "11111111"};
 
-    for(int i = 0; i < 8; i++){
+    for(int i = 0; i < 9; i++){
         char command[50];
             snprintf(command, sizeof(command), "./countone %s", binaries_to_test[i]);
             system(command);

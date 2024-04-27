90661d7b27ac0569ad0e9fd3d041d81fef1f8b68
Franky03 <kaikym20@gmail.com>
2024-04-27 13:26:50 -0300

    comecando essa MIZERAA

---
+++ Assembly/helloworld.asm
@@ -0,0 +1,15 @@
+.global _start
+_start:
+    MOV R0, #1 @ syscall number for sys_write
+    LDR R1, =message @ address of the message
+    LDR R2, =len @ length of the message
+    MOV R7, #4 @ syscall number for sys_write
+    SWI 0 @ make the syscall
+
+    MOV R7, #1 @ syscall number for sys_exit
+    SWI 0 @ make the syscall
+
+.data 
+message:
+    .asciz "Hello, World!\n"
+len = . - message
